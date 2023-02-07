d35a8c7c9d1b40f6cf1d5e12683ef798fcaf1043
Muhammed zaeem razi <mzrazi7@gmail.com>
2023-02-08 03:22:36 +0530

    user added to signup too

--- controllers/user.js
+++ controllers/user.js
@@ -18,7 +18,7 @@ createUser : (req, res) => {
         return res.status(400).json({ error });
         
       }
-      return res.status(201).json({ message: 'User created successfully' });
+      return res.status(201).json({ message: 'User created successfully' ,user});
     });
   });
 },
