f10ed0b6278bc9bdf484b3dc5809499be728fbbe
Muhammed zaeem razi <mzrazi7@gmail.com>
2023-02-09 01:36:25 +0530

    the updated createuser function

--- controllers/user.js
+++ controllers/user.js
@@ -4,72 +4,113 @@ const nodemailer = require('nodemailer');
 var jwt = require('jsonwebtoken');
 
 module.exports={
-  createUser: (req, res) => {
-    var userdata = req.body;
-    console.log(userdata);
-    bcrypt.hash(userdata.Password, 10, (err, hash) => {
-      if (err) {
-        return res.status(500).json({ error: err });
-      }
-      const user = new User({
-        Name: userdata.Name,
-        Address: userdata.Address,
-        Phone: userdata.Phone,
-        Email: userdata.Email,
-        Password: hash,
-        emailverified: false
+  createUser: async (req, res) => {
+    try {
+      const userdata = req.body;
+      console.log(userdata);
+    
+      // Check if the email already exists and is verified
+      const existingVerifiedUser = await User.findOne({ 
+        Email: userdata.Email, 
+        emailverified: true 
       });
-      console.log(user);
-      user.save((error) => {
-        if (error) {
-          console.log(error);
-          return res.status(400).json({ error: error.message });
-        }
+      if (existingVerifiedUser) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+    
+      // Check if the email already exists but is not verified
+      let existingUser = await User.findOne({ 
+        Email: userdata.Email, 
+        emailverified: false 
+      });
+    
+      if (existingUser) {
+        // Generate a new token
+        const token = jwt.sign({ email: userdata.Email }, process.env.SECRET_KEY, {
+          expiresIn: "1h"
+        });
+        // Send a new email with the new token
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.zoho.in',
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.MAILER_EMAIL,
+            pass: process.env.MAILER_PASSWORD
+          }
+        });
+    
+        const mailOptions = {
+          from: '"masjid app" masjidappcc@gmail.com',
+          to: userdata.Email,
+          subject: 'Verify your email address',
+          text: `Please click the following link to verify your email address: http://localhost:3000/verify-email/${token}`
+        };
+    
+        await transporter.sendMail(mailOptions);
+        return res.status(201).json({ 
+          message: 'User already exists, a new verification email has been sent', 
+          user: existingUser 
+        });
+      } else {
+        // Hash the password
+        const hash = await bcrypt.hash(userdata.Password, 10);
+        // Create a new user
+        const user = new User({
+          Name: userdata.Name,
+          Address: userdata.Address,
+          Phone: userdata.Phone,
+          Email: userdata.Email,
+          Password: hash,
+          emailverified: false
+        });
+    
+        // Save the user
+        await user.save();
+    
         // Generate a token
         const token = jwt.sign({ email: userdata.Email }, process.env.SECRET_KEY, {
           expiresIn: "1h"
-        }); 
+        });
         // Send an email with the token
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.zoho.in',
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.MAILER_EMAIL,
+            pass: process.env.MAILER_PASSWORD
+          }
+        });
+        const mailOptions = {
+          from: '"masjid app" masjidappcc@gmail.com',
+          to: userdata.Email,
+          subject: 'Verify your email address',
+          text: `Please click the following link to verify your email address: http://localhost:3000/verify-email/${token}`
+        };
+        
+        await transporter.sendMail(mailOptions);
+        
+        return res.status(201).json({ 
+          message: 'User created, verification email sent', 
+          user 
+        });
+      }
+     }catch (error) {
+            return res.status(500).json({ message: error.message });
+       }
+        },
+        
+    
+       
+  
 
-      
-
-
-const transporter = nodemailer.createTransport({
-  host: 'smtp.zoho.in',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.MAILER_EMAIL,
-    pass: process.env.MAILER_PASSWORD
-  } 
-});
-console.log(process.env.MAILER_EMAIL);
-
-const mailOptions = {
-  from: '"masjid app" masjidappcc@gmail.com',
-  to: userdata.Email,
-  subject: 'Verify your email address',
-  text: `Please click the following link to verify your email address: http://3.7.71.236:3000/verify-email/${token}`
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.log(error); 
-    return res.status(500).json({ error });
-  }
-  return res.status(201).json({ message: 'User created successfully, an email has been sent to verify the email address', user });
-});
-
-      
-      });
-    });
-  },
   verifyEmail: (req, res) => {
     const token = req.params.token;
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
       if (err) {
         if (err.name === "TokenExpiredError") {
-          User.findOneAndDelete({ Email: decoded.email }, (err, user) => {
+          User.findOneAndDelete({ email: decoded.Email }, (err, user) => {
             if (err) {
               return res.status(500).json({ error: err, message: "Deletion error" });
             }
