260d8c013f9c62d3b20c01175c864895c0a10711
Muhammed zaeem razi <mzrazi7@gmail.com>
2023-02-27 16:39:39 +0530

    removed gender require true

--- models/usermodel.js
+++ models/usermodel.js
@@ -20,7 +20,7 @@ const FamilySchema = new Schema({
     },
     Gender:{
       type:String,
-      required:true
+     
     },
     User: {
       type: Schema.Types.ObjectId,
@@ -43,10 +43,87 @@ const userSchema = new Schema({
    resetTokenExpiration:{type:String},
    emailverified: { type: Boolean, default: false},
    Family: [{ type: Schema.Types.ObjectId, ref: 'Family' }]
+})
+
+const PaymentStatusSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  jan: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  feb: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  mar: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  apr: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  may: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  jun: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  jul: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  aug: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  sep: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  oct: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  nov: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
+  dec: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'due', 'paid'],
+  },
 });
+
+
+
+
 
 module.exports = {
     Family: mongoose.model("Family", FamilySchema),
     User: mongoose.model('User', userSchema),
+   payments: mongoose.model('PaymentStatus', PaymentStatusSchema)
   };
   
